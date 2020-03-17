dff0aa8c852475ca9ea12f025126bb8a902679d9
tram0808 <phanquynhbichtram88@gmail.com>
2020-03-17 15:33:38 +0700

    flexDirection 3

--- App.js
+++ App.js
@@ -7,55 +7,37 @@ class MyClass extends Component {
   render() {
     return (
       <View style={styles.container}>
-        {/* <View style={styles.first}>
+        <View style={styles.first}>
           <View style={styles.text1}>
-            <Text style={styles.text}>A</Text>
+            <Text style={styles.text}>1</Text>
           </View>
           <View style={styles.text2}>
-            <Text style={styles.text}>B</Text>
-          </View>
-          <View style={styles.text3}>
-            <Text style={styles.text}>C</Text>
-          </View>
-          <View style={styles.text4}>
-            <Text style={styles.text}>D</Text>
+            <Text style={styles.text}>2</Text>
           </View>
         </View>
         <View style={styles.second}>
-          <View style={styles.text1}>
-            <Text style={styles.text}>A</Text>
-          </View>
-          <View style={styles.text2}>
-            <Text style={styles.text}>B</Text>
-          </View>
-          <View style={styles.text3}>
-            <Text style={styles.text}>C</Text>
-          </View>
-          <View style={styles.text5}>
-            <Text style={styles.text}>D</Text>
-          </View>
-        </View> */}
-        <View style={styles.group1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.group2}>
-          <View style={styles.left}>
-            <Text style={styles.text}>2</Text>
-          </View>
-          <View style={styles.right}>
-            <View style={styles.text1}>
+          <View style={styles.group1}>
+            <View style={styles.text3}>
               <Text style={styles.text}>3</Text>
             </View>
-            <View style={styles.text2}>
-              <View style={styles.text3}>
-                <Text style={styles.text}>4</Text>
-              </View>
-              <View style={styles.text4}>
-                <Text style={styles.text}>5</Text>
-              </View>
+            <View style={styles.text4}>
+              <Text style={styles.text}>4</Text>
             </View>
           </View>
+          <View style={styles.group2}>
+            <Text style={styles.text}>5</Text>
+          </View>
+
         </View>
+        <View style={styles.third}>
+          <View style={styles.text6}>
+            <Text style={styles.text}>6</Text>
+          </View>
+          <View style={styles.text7}>
+            <Text style={styles.text}>7</Text>
+          </View>
+
+        </View>       
       </View>
     );
   }
@@ -67,110 +49,90 @@ const styles = StyleSheet.create({
     flex: 10,
     flexDirection:'column',
     borderColor: 'black',
-    borderWidth: 3,
+    borderWidth: 5,
   },
-  // first:{
-  //   flex:1,
-  //   flexDirection:'column',
-
-  // },
-  // text1:{
-  //   flex:1,
-  //   backgroundColor:'blue',
-  //   alignItems:'center',
-  //   justifyContent:'center',
-  // },
-  // text2:{
-  //   flex:1,
-  //   backgroundColor:'brown',
-  //   alignItems:'center',
-  //   justifyContent:'center',
-  // },
-  // text3:{
-  //   flex:1,
-  //   backgroundColor:'pink',
-  //   alignItems:'center',
-  //   justifyContent:'center',
-  // },
-  // text4:{
-  //   flex:1,
-  //   backgroundColor:'#ffe4c4',
-  //   alignItems:'center',
-  //   justifyContent:'center',
-  // },
-  // text5:{
-  //   flex:1,
-  //   backgroundColor:'#deb887',
-  //   alignItems:'center',
-  //   justifyContent:'center',
-  // },
-  // text:{
-  //   fontSize:25,
-  // },
-  // second:{
-  //   flex:1,
-  //   flexDirection:'row',
-  //   backgroundColor:'pink',
-  // },
-
-  group1:{
-    flex:3,
-    backgroundColor:'red',
-    alignItems:'center',
-    justifyContent:'center',
-    borderColor: 'black',
-    borderBottomWidth: 3,
-  },
-  
-  group2:{
-    flex:7,
-    flexDirection:'row',
-  },
-  left:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    flexDirection:'row',
-    backgroundColor:'yellow',
-    borderColor: 'black',
-    borderRightWidth:3,
-  },
-  right:{
+  first:{
     flex:2,
-   
-    flexDirection:'column',
+    flexDirection:'row',
+    
+    
   },
   text1:{
-    flex:6,
-    backgroundColor:'blue',
+    backgroundColor:'red',
+    flex:3,
     alignItems:'center',
     justifyContent:'center',
     borderColor: 'black',
-    borderBottomWidth:3,
+    borderBottomWidth:5,
+    borderRightWidth:5,
   },
   text2:{
-    flex:4,
+    backgroundColor:'yellow',
+    flex:7,
+    alignItems:'center',
+    justifyContent:'center',
+    borderColor: 'black',
+    borderBottomWidth:5,
+  },
+  second:{
+    flex:6,
     flexDirection:'row',
+  },
+  group1:{
+    flexDirection:'column',
+    flex:7,
+  },
+  group2:{
+    flex:3,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#d8bfd8',
+    borderColor: 'black',
+    borderBottomWidth:5,
+    borderLeftWidth:5,
 
   },
   text3:{
     flex:1,
-    backgroundColor:'pink',
+    backgroundColor:'green',
     alignItems:'center',
     justifyContent:'center',
     borderColor: 'black',
-    borderRightWidth:3,
+    borderBottomWidth:5,
+   
   },
   text4:{
     flex:1,
-    backgroundColor:'#9400d3',
+    backgroundColor:'#ee82ee',
+    alignItems:'center',
+    justifyContent:'center',
+    borderColor: 'black',
+    borderBottomWidth:5,
+  },
+  third:{
+    flex:2,
+    flexDirection:'row',
+    backgroundColor:'red',
+  },
+  text6:{
+    flex:1,
+    backgroundColor:'salmon',
+    alignItems:'center',
+    justifyContent:'center',
+    borderColor: 'black',
+    borderRightWidth:5,
+  },
+  text7:{
+    flex:1,
+    backgroundColor:'powderblue',
     alignItems:'center',
     justifyContent:'center',
   },
+
   text:{
-    fontSize:30,
+    fontSize:50,
+    fontWeight:'700',
     color:'white',
-    fontWeight:'600'
   }
 
 });
